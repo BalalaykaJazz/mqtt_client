f2e1fa8def5d8d45c32faee4a66392430b744e07
Balalaykajazz <archer2000@mail.ru>
2021-10-14 13:15:03 +0300

    исправлены ошибки при декодировании

--- scripts/decode_env.sh
+++ scripts/decode_env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Декодирование настроек
 
+# topics.json
 scp ../src/mqtt_client/settings/enc_topics.json ../src/mqtt_client/settings/topics.json
 sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/iot  --verbose -d -i ../src/mqtt_client/settings/topics.json
 
+# .env
 sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/iot  --verbose -d -i ../src/mqtt_client/settings/enc_env
 scp ../src/mqtt_client/settings/enc_env ../src/mqtt_client/settings/.env
