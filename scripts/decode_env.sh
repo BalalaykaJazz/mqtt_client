c7113de8f30dbc94ed2f3a4499ee7b6e7553c137
Balalaykajazz <archer2000@mail.ru>
2021-10-14 12:23:10 +0300

    Добавлен скрипт для расшифровки настроек

--- scripts/decode_env.sh
+++ scripts/decode_env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Декодирование настроек
 
-sops -d src/mqtt_client/settings/enc_topics.json > src/mqtt_client/settings/topics.json
-sops -d src/mqtt_client/settings/enc_env > src/mqtt_client/settings/.env
+scp ../src/mqtt_client/settings/enc_env ../src/mqtt_client/settings/.env
+scp ../src/mqtt_client/settings/enc_topics.json ../src/mqtt_client/settings/topics.json
+sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/iot  --verbose -d -i ../src/mqtt_client/settings/.env
+sops --hc-vault-transit $VAULT_ADDR/v1/sops/keys/iot  --verbose -d -i ../src/mqtt_client/settings/topics.json
